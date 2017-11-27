80850e0206e0add7120b8822d052dc93418f9ba4
Tibor Tarnai <tibor.tarnai@sap.com>
2017-11-27 11:23:53 +0100

    fix tabs

--- install-code-formatter.sh
+++ install-code-formatter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#register hooks path
 mkdir -p ~/hooks/
 cp pre-commit ~/hooks/
 git config --global core.hooksPath ~/hooks/
@@ -7,10 +8,10 @@ git config --global core.hooksPath ~/hooks/
 #install google-java-format
 rm -rf google-java-format
 git clone https://github.com/google/google-java-format.git
-
 cd google-java-format
-mvn -q clean install
+mvn clean install
 mv core/target/google-java-format-*-all-deps.jar ~/hooks/google-java-format.jar
+
 #install js-beautify
 npm -g uninstall js-beautify
 npm -g install js-beautify
